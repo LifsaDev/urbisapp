34f02eadf5a873a3d73d20c809d26253a38adf58
LifsaDev <salifousaouadogo@gmail.com>
2025-03-04 07:57:13 +0000

    update

--- src/services/configs.js
+++ src/services/configs.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const isDevelopment = import.meta.env.MODE === 'development'
 //variables
 const Backend_URL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_DEV : import.meta.env.VITE_API_BASE_URL_PROD
-const Urbis_App_URL = "https://www.urbisapp.com/"  //isDevelopment ? import.meta.env.VITE_URBIS_SITE_URL_DEV : import.meta.env.VITE_URBIS_SITE_URL_PROD
+const Urbis_App_URL = "https://www.app-urbis.com/"  //isDevelopment ? import.meta.env.VITE_URBIS_SITE_URL_DEV : import.meta.env.VITE_URBIS_SITE_URL_PROD
  
 const api = axios.create({
   baseURL: "https://urbis-api-fkc7b8atdnhhhhd5.westeurope-01.azurewebsites.net/api/",
